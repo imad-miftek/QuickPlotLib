1b6a9a4fb79e68b95822a97118adea74fe2e2ee6
imad-miftek <imad@miftek.com>
2025-12-07 17:57:14 -0500

    better structure gallery loads,

---
+++ QuickPlotLib/Plugin.cpp
@@ -0,0 +1,20 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2025 QuickPlotLib contributors
+// SPDX-License-Identifier: MIT
+
+#include <QtQml/qqmlextensionplugin.h>
+
+extern void qml_register_types_QuickPlotLib();
+Q_GHS_KEEP_REFERENCE(qml_register_types_QuickPlotLib)
+
+class QuickPlotLibPlugin : public QQmlEngineExtensionPlugin {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID QQmlEngineExtensionInterface_iid)
+
+   public:
+    QuickPlotLibPlugin(QObject *parent = nullptr) : QQmlEngineExtensionPlugin(parent) {
+        volatile auto registration = &qml_register_types_QuickPlotLib;
+        Q_UNUSED(registration)
+    }
+};
+
+#include "Plugin.moc"
